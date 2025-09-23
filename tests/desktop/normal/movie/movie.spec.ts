e5e9f4cb2344fb1d0e34f2b8aee3bd8d53286e1d
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2025-09-23 08:20:13 -0400

    rm test.only

--- tests/desktop/normal/movie/movie.spec.ts
+++ tests/desktop/normal/movie/movie.spec.ts
@@ -2,7 +2,7 @@ import { test } from "@playwright/test";
 import { Helioviewer } from "../../../page_objects/helioviewer";
 
 // If there is no image to make movie , then hv should show some friendly notification message
-test.only("If there is not enough data, helioviewer should show warning message about no data situation", async ({
+test("If there is not enough data, helioviewer should show warning message about no data situation", async ({
   page
 }) => {
   let hv = new Helioviewer(page);
